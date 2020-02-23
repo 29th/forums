f2b70a4c4a6258f982fa4bda25d6e7bb7e11c1ab
wilson29thid <wilson@29th.org>
2020-02-23 16:05:55 +0000

    Fix Unread on Index plugin. Closes 29th/personnel#440

--- plugins/UnreadOnIndex/class.unreadonindex.plugin.php
+++ plugins/UnreadOnIndex/class.unreadonindex.plugin.php
@@ -1,22 +1,8 @@
-<?php if (!defined('APPLICATION')) exit();
-
-$PluginInfo['UnreadOnIndex'] = array(
-	'Name' => 'UnreadOnIndex',
-	'Description' => 'Show new/unread tags on category index',
-	'Version' => '1.0',
-	'MobileFriendly' => TRUE,
-	'Author' => 'Wilson29thID',
-	'AuthorEmail' => 'wilson@29th.org',
-	'AuthorUrl' => 'http://29th.org',
-	'License' => 'MIT'
-);
-
-class UnreadOnIndex extends Gdn_Plugin {
-    
-    public function Base_AfterCategoryTitle_Handler($Sender, $Args) {
-        if( ! $Args['Category']['Read']) {
+<?php
+class UnreadOnIndexPlugin extends Gdn_Plugin {
+    public function categoriesController_afterCategoryTitle_handler($sender, $args) {
+        if( ! $args['Category']['Read']) {
             echo ' <strong class="HasNew JustNew NewCommentCount" title="You haven\'t read this yet.">new</strong>';
         }
     }
-
 }
